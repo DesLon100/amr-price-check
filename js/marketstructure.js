54375a0aedfe0f97e4afa2aed3cbb273ccae5976
DesLon100 <s.duthy@artmarketresearch.com>
2026-02-23 20:43:20 +0000

    Update marketstructure.js

--- js/marketstructure.js
+++ js/marketstructure.js
@@ -1,1 +1,11 @@
+import { renderMarketStructure } from "./marketstructure.js";
 
+renderMarketStructure({
+  workbench,
+  artistId,
+  elTop: document.getElementById("pc-structure-top"),
+  elBottom: document.getElementById("pc-structure-bottom"),
+  toggleBtn: document.getElementById("pc-structure-toggle"),
+  panelEl: document.getElementById("pc-structure-panel"),
+  noteEl: document.getElementById("pc-structure-note")
+});
